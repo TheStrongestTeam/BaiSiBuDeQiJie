24e939ad06d3b54d58b87bd675e899052dea0dd8
王国亮 <leavessilent@gmail.com>
2016-09-21 15:22:12 +0800

    精华段子布局

--- app/src/main/java/com/miraclehu/baisibudeqijie/common/HttpConstants.java
+++ app/src/main/java/com/miraclehu/baisibudeqijie/common/HttpConstants.java
@@ -4,7 +4,9 @@ package com.miraclehu.baisibudeqijie.common;
  * Created by user on 2016/9/20.
  */
 public class HttpConstants {
-    public static final String LASTEST_ALL_BEGIN="http://s.budejie.com/topic/list/zuixin/1/budejie-android-6.5.4/";
-    public static final String LASTEST_ALL_END="-20.json";
+    public static final String LASTEST_ALL_BEGIN = "http://s.budejie.com/topic/list/zuixin/1/budejie-android-6.5.4/";
+    public static final String LASTEST_ALL_END = "-20.json";
+
+    public static final String URL_ESSENCE_JOKE = "http://s.budejie.com/topic/tag-topic/64/hot/budejie-android-6.5.1/0-20.json";
 
 }
--- app/src/main/java/com/miraclehu/baisibudeqijie/ui/fragment/essence/EssenceJokeFragment.java
+++ app/src/main/java/com/miraclehu/baisibudeqijie/ui/fragment/essence/EssenceJokeFragment.java
@@ -1,15 +1,28 @@
 package com.miraclehu.baisibudeqijie.ui.fragment.essence;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.miraclehu.baisibudeqijie.R;
 import com.miraclehu.baisibudeqijie.ui.fragment.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.appsdream.nestrefresh.normalstyle.NestRefreshLayout;
 
 /**
  * Created by user on 2016/9/20.
  */
 public class EssenceJokeFragment extends BaseFragment {
     public static final String TAG = EssenceJokeFragment.class.getSimpleName();
+    @BindView(R.id.essence_joke_recycler_view)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.essence_joke_refresh_layout)
+    NestRefreshLayout mRefreshLayout;
+
 
     @Override
     public int getLayoutResId() {
