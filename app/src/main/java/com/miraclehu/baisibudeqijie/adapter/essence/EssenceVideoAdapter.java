38f62f8daf5092585c5bbb396aee64a336cb1606
王国亮 <leavessilent@gmail.com>
2016-09-23 11:18:25 +0800

    动态设置精选视频界面 视频的宽高等，优化布局

--- app/src/main/java/com/miraclehu/baisibudeqijie/adapter/essence/EssenceVideoAdapter.java
+++ app/src/main/java/com/miraclehu/baisibudeqijie/adapter/essence/EssenceVideoAdapter.java
@@ -1,15 +1,19 @@
 package com.miraclehu.baisibudeqijie.adapter.essence;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.leavessilent.mylibrary.adapters.UniversalAdapter;
 import com.leavessilent.mylibrary.utils.ImageHelper;
+import com.leavessilent.mylibrary.utils.ScreenHelper;
 import com.miraclehu.baisibudeqijie.R;
 import com.miraclehu.baisibudeqijie.model.U;
+import com.miraclehu.baisibudeqijie.model.Video;
 import com.miraclehu.baisibudeqijie.model.VideoList;
 
 import java.util.List;
@@ -21,10 +25,16 @@ import jp.wasabeef.glide.transformations.CropCircleTransformation;
  */
 public class EssenceVideoAdapter extends UniversalAdapter<VideoList> {
     private Context mContext;
+    private final int mMaxWidth;
+    private final int mMaxHeight;
+
 
     public EssenceVideoAdapter(Context context, List<VideoList> data) {
         super(context, data);
         mContext = context;
+        // 减去两边的margin值
+        mMaxWidth = ScreenHelper.getScreenWidth() - ScreenHelper.dp2px(30);
+        mMaxHeight = ScreenHelper.getScreenHeight() * 3 / 5;
     }
 
     public EssenceVideoAdapter(Context context) {
@@ -50,6 +60,22 @@ public class EssenceVideoAdapter extends UniversalAdapter<VideoList> {
         TextView forward = holder.getTextView(R.id.share);
         TextView comment = holder.getTextView(R.id.common);
 
+        View layout = holder.getView(R.id.layout);
+
+        Video video = videoList.getVideo();
+        // 缩放比
+        float widthScale = (float) mMaxWidth / video.getWidth();
+        float heightScale = (float) mMaxHeight / video.getHeight();
+        ViewGroup.LayoutParams params = layout.getLayoutParams();
+        if (widthScale < heightScale) {
+            params.width = mMaxWidth;
+            params.height = ((int) (widthScale * video.getHeight() + 0.5f));
+        } else {
+            params.height = mMaxHeight;
+            params.width = ((int) (heightScale * video.getWidth() + 0.5f));
+        }
+        layout.setLayoutParams(params);
+
         U u = videoList.getU();
         CropCircleTransformation transformation = new CropCircleTransformation(mContext);
         Glide.with(mContext).load(u.getHeader().get(0)).bitmapTransform(transformation).into(icon);
@@ -62,13 +88,13 @@ public class EssenceVideoAdapter extends UniversalAdapter<VideoList> {
         String passtime = videoList.getPasstime();
         time.setText(passtime.substring(0, passtime.length() - 3));
         title.setText(videoList.getText());
-        ImageHelper.display(preview, videoList.getVideo().getThumbnail().get(0));
+        ImageHelper.display(preview, video.getThumbnail().get(0));
         play.setOnClickListener(this);
 
-        up.setText(videoList.getUp());
-        down.setText(videoList.getDown());
-        forward.setText(videoList.getForward());
-        comment.setText(videoList.getComment());
+        up.setText(" " + String.valueOf(videoList.getUp()));
+        down.setText(" " + String.valueOf(videoList.getDown()));
+        forward.setText(" " + String.valueOf(videoList.getForward()));
+        comment.setText(" " + String.valueOf(videoList.getComment()));
 
     }
 
--- app/src/main/java/com/miraclehu/baisibudeqijie/ui/fragment/essence/EssenceVideoFragment.java
+++ app/src/main/java/com/miraclehu/baisibudeqijie/ui/fragment/essence/EssenceVideoFragment.java
@@ -1,15 +1,38 @@
 package com.miraclehu.baisibudeqijie.ui.fragment.essence;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+import com.leavessilent.mylibrary.utils.HttpUtils;
 import com.miraclehu.baisibudeqijie.R;
+import com.miraclehu.baisibudeqijie.adapter.essence.EssenceVideoAdapter;
+import com.miraclehu.baisibudeqijie.common.HttpConstants;
+import com.miraclehu.baisibudeqijie.model.VideoList;
+import com.miraclehu.baisibudeqijie.model.VideoRoot;
 import com.miraclehu.baisibudeqijie.ui.fragment.BaseFragment;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.appsdream.nestrefresh.normalstyle.NestRefreshLayout;
 
 /**
  * Created by user on 2016/9/20.
  */
 public class EssenceVideoFragment extends BaseFragment {
     public static final String TAG = EssenceVideoFragment.class.getSimpleName();
+    @BindView(R.id.essence_video_recycler_view)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.essence_video_refresh)
+    NestRefreshLayout mRefreshLayout;
+    private EssenceVideoAdapter mAdapter;
 
     @Override
     public int getLayoutResId() {
@@ -18,6 +41,29 @@ public class EssenceVideoFragment extends BaseFragment {
 
     @Override
     protected void initView(Bundle savedInstanceState) {
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(manager);
+        mAdapter = new EssenceVideoAdapter(getContext());
+        mRecyclerView.setAdapter(mAdapter);
 
+        getDataFromInternet();
     }
+
+    private void getDataFromInternet() {
+        HttpUtils.getAsynString(HttpConstants.URL_ESSENCE_VIDEO, new HttpUtils.ResultCallback() {
+            @Override
+            public void onFailure(Exception e) {
+
+            }
+
+            @Override
+            public void onFinish(String result) {
+                Gson gson = new Gson();
+                VideoRoot root = gson.fromJson(result, VideoRoot.class);
+                List<VideoList> list = root.getList();
+                mAdapter.updateRes(list);
+            }
+        });
+    }
+
 }
